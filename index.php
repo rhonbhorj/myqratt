64794f01b9159a024d3b994fe0225dfc579b8087
rhon bhorj <rhonbhorj@gmail.com>
2023-08-11 15:10:05 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>CodePen - Login responsive</title>
+    <link rel="stylesheet" href="./style.css" />
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+</head>
+
+<body>
+    <!-- partial:index.partial.html -->
+
+    <body>
+        <script src="https://kit.fontawesome.com/ab00d86059.js" crossorigin="anonymous"></script>
+        <!-- <img
+        class="wave"
+        src="https://raw.githubusercontent.com/Jhonierpc/WebDevelopment/master/Login%20Responsive/img/wave.png"
+        alt=""
+      /> -->
+        <div class="container">
+            <div class="img">
+                <img src="img/images.png" alt="" />
+            </div>
+            <div class="login-container">
+                <form action="index.html">
+                    <img class="avatar" src="img/logo.jpg" alt="" />
+                    <h2>Qr-Attendance</h2>
+                    <div class="input-div one">
+                        <div class="i">
+                            <i class="fas fa-user"></i>
+                        </div>
+                        <div>
+                            <h5>Username</h5>
+                            <input class="input" type="text" />
+                        </div>
+                    </div>
+                    <div class="input-div two">
+                        <div class="i">
+                            <i class="fas fa-lock"></i>
+                        </div>
+                        <div>
+                            <h5>Password</h5>
+                            <input class="input" type="password" />
+                        </div>
+                    </div>
+                    <a href="#">Forgot Pasword</a>
+                    <input class="btn" type="submit" value="Login" />
+                </form>
+            </div>
+        </div>
+
+    </body>
+
+
+    <script type="text/javascript">
+
+    </script>
+    <!-- partial -->
+    <script src="script.js"></script>
+</body>
+
+</html>
